f0f58b4aaa14724e0a216a140f20d0b97c552779
danywalls <danywalls4@gmail.com>
2023-09-21 17:31:58 +0200

    Initial commit

---
+++ src/services/cards.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+export type Card = {
+  id: string;
+  holder: string;
+  status: string;
+};
+
+@Injectable({ providedIn: 'root' })
+export class CardsService {
+  cards: Array<Card> = [];
+  cardsleft: number = 3;
+
+  add(holder: string) {
+    const card = {
+      id: Math.random().toFixed(),
+      holder,
+      status: 'pending',
+    };
+    this.cards = [...this.cards, card];
+    this.cardsleft = this.cardsleft - 1;
+  }
+}
