85dfa0d509f8ae554f95cc27c8b18eab353248d8
danywalls <danywalls4@gmail.com>
2023-09-23 11:17:41 +0200

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,29 @@
 import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { CardsService } from './services/cards.service';
 
 @Component({
   selector: 'my-app',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular
-    </a>
+   
+   <input type="text" #holder />
+   <button (click)="add(holder.value)">Save</button>
   `,
 })
 export class App {
+  
   name = 'Angular';
+  cardsService = inject(CardsService)
+  cards = this.cardsService.cards;
+  cardLeft = this.cardsService.cardsleft;
+  
+  add(holder: string){
+    this.cardsService.add(holder);
+  }
 }
 
 bootstrapApplication(App);
